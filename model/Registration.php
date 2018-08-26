cf9088d2e512a75da3d035eb8848d027905244eb
Caroline Moulin <carolinemoulin84@gmail.com>
2018-08-26 10:04:36 +0200

    Suppression d'une fonction en doublon

--- model/Registration.php
+++ model/Registration.php
@@ -2,18 +2,6 @@
 require_once("model/Manager.php");
 class UserManager extends Manager
 {
-    //passage en parametre des valeurs du formulaire et du mdp haché
-    public function addMember($pseudo, $pass_hash, $email)
-    {
-        $db = $this->dbConnect();
-        $req = $db->prepare('INSERT INTO members(pseudo, pass, email, inscription_date) VALUES(:pseudo, :pass, :email, CURDATE())');
-        //On rempli la BDD avec les infos du formulaire
-        $req->execute(array(
-            'pseudo' => $pseudo,
-            'pass' => $pass_hash,
-            'email' => $email));
-    }
-
     public function checkPseudo($pseudoToCheck)
     {
         $db = $this->dbConnect();
